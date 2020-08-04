81033d0aa8aa817ba6c532ae61d6927bb1fd9e94
onekit-qing <64195283+onekit-qing@users.noreply.github.com>
2020-08-04 14:39:49 +0800

    .

---
+++ uniapp_weixin/onekit/uni.js
@@ -0,0 +1,8 @@
+export default class uni {
+  static base64ToArrayBuffer(base64) {
+    return wx.base64ToArrayBuffer(base64)
+  }
+  static arrayBufferToBase64(arrayBuffer) {
+    return wx.arrayBufferToBase64(arrayBuffer)
+  }
+}
---
+++ uniapp_weixin/pages/index/index.js
@@ -0,0 +1,20 @@
+import uni from '../../onekit/uni.js'
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+
+  onLoad: function () {
+    // const base64 = 'CxYh'
+    // const arrayBuffer = uni.base64ToArrayBuffer(base64)
+    // console.log("base64ToArrayBuffer", arrayBuffer)
+
+    // const arrayBuffer = new Uint8Array([55, 55, 55])
+    // const base64 = wx.arrayBufferToBase64(arrayBuffer)
+    // console.log("arrayBufferToBase64", base64)
+
+  },
+
+})
